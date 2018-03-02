ae70d36ee4c8a2e5f7a168280df44bc09caa36c5
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-03-02 16:50:32 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,k;
+char a[i];
+ptintf("enter rhe string);
+scanf("%s",a);
+printf("enter the number");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("%c",a[i]);
+}
+}
