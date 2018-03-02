441ed985a85d523bfb36a4d3fa811ed6c75c61da
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-03-02 15:56:32 +0530

    Create sum7.c

---
+++ sum7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,m,a;
+printf("enter the two numbers:");
+scanf("%d %d",&n,&m);
+a=n+m;
+if(a%2==0)
+{
+printf("\neven");
+}
+else
+{
+printf("\nodd");
+}
+}
+
