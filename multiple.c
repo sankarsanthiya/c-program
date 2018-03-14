3a1566695bf9f9827980c0192d4a704fb297eb43
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-03-14 16:11:51 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
