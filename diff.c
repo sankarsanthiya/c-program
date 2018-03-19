dc3e7b55ca0268842752d0099e5e6c2f15cc7188
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-03-19 16:31:12 +0530

    Rename sub.c to diff.c

---
+++ diff.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i;
+    printf("enter the two numbers:");
+    for(i=1;i<=3;i++)
+    {
+        scanf("%d %d",&a,&b);     
+        printf("%d",b-a);
+    }
+    
+}
